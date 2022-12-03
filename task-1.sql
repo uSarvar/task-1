8fcf1ea8259dbf3d62a950c53c27973d8c2b4a72
Sarvar Anvarovich <61342503+uSarvar@users.noreply.github.com>
2022-12-03 17:10:29 +0500

    Update task-1.sql

--- task-1.sql
+++ task-1.sql
@@ -1,1 +1,14 @@
+CREATE DATABASE clients-info_db;
 
+CREATE TABLE customers (
+  customerNumber int(15) NOT NULL,
+  customerName varchar(33) NOT NULL,
+  contactLastName varchar(33) NOT NULL,
+  contactFirstName varchar(33) NOT NULL,
+  phone varchar(33) NOT NULL UNIQUE,
+  addressLine1 varchar(100),
+  addressLine2 varchar(100),
+  
+);
+
+INSERT INTO customers VALUES ('Sarvar', 'Urazbaev', ');
